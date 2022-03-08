e239b84e2739d090ffb9e3a7bc95323db268e0ca
3takita <98113361+3takita@users.noreply.github.com>
2022-03-08 15:57:10 -0800

    Light-dark toggle challenge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import "./styles.css";
+import Navbar from "./Navbar";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <Navbar />
+      <Main />
     </div>
   );
 }
